d96ee5a1318408ae44025454a1b8766df98d3eaf
davidfabian <fabiand.01@gmail.com>
2017-09-17 14:14:09 +0300

    search button and search keyboard search icon both works with dummy data

--- app/src/main/java/com/example/d/booksearch/MainActivity.java
+++ app/src/main/java/com/example/d/booksearch/MainActivity.java
@@ -15,18 +15,41 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SearchView searchView = (SearchView) findViewById(R.id.searchview_volume);
-        Button searchButton = (Button) findViewById(R.id.button_search);
+        final SearchView searchView = (SearchView) findViewById(R.id.searchview_volume);
+        final Button searchButton = (Button) findViewById(R.id.button_search);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                //Log.e("onQueryTextChange", "called");
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+
+                TextView dummyone = (TextView) findViewById(R.id.DummyTextView);
+                dummyone.setText(searchView.getQuery());
+                dummyone.setVisibility(View.VISIBLE);
+                // Do your task here
+
+                return false;
+            }
+
+        });
+
 
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //testing button onclick
                 TextView dummyone = (TextView) findViewById(R.id.DummyTextView);
+                dummyone.setText(searchView.getQuery());
                 dummyone.setVisibility(View.VISIBLE);
-
             }
         });
-
     }
+
+
 }
