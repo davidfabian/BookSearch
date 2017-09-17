c3bbbc99c1e7bd8f2bba31b261dfbb15670eb891
davidfabian <fabiand.01@gmail.com>
2017-09-17 13:33:43 +0300

    search button now clickable, displays dummy message on screen.

--- app/src/main/java/com/example/d/booksearch/MainActivity.java
+++ app/src/main/java/com/example/d/booksearch/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.d.booksearch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SearchView searchView = (SearchView) findViewById(R.id.searchview_volume);
+        Button searchButton = (Button) findViewById(R.id.button_search);
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //testing button onclick
+                TextView dummyone = (TextView) findViewById(R.id.DummyTextView);
+                dummyone.setVisibility(View.VISIBLE);
+
+            }
+        });
+
     }
 }
